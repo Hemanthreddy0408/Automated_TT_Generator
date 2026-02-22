c1bc783ed260d6b5c8eedee794c12cdcf8eb58fd
ravi <ravikarthikeya1825@gmail.com>
2026-02-22 22:53:08 +0530

    timetable optimisation added notification

---
+++ Automated-Timetable-Scheduling-and-Faculty-Workload-Optimization-System/backend/scheduler/src/main/java/com/acadschedule/scheduler/entity/Notification.java
@@ -0,0 +1,60 @@
+package com.acadschedule.scheduler.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/*
+ * ---------------------------------------------------------
+ * Notification Entity
+ * ---------------------------------------------------------
+ * Stores system notifications after optimization.
+ * ---------------------------------------------------------
+ */
+@Entity
+@Table(name = "notifications")
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+
+    private Boolean readStatus = false;
+
+    private LocalDateTime createdAt;
+
+    // Getters & Setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Boolean getReadStatus() {
+        return readStatus;
+    }
+
+    public void setReadStatus(Boolean readStatus) {
+        this.readStatus = readStatus;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
